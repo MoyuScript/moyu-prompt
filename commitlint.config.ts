987431018d903893ef438fffadda61b521e955bd
MoyuScript <i@moyu.moe>
2021-12-04 21:01:55 +0800

    feat: prettier

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,10 @@
-import type {UserConfig} from '@commitlint/types';
+import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [0],
-  }
+  },
 };
 
-export default config
+export default config;
